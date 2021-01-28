d7a0389c9c5fabf195988f8374977f6f9d2650ac
Alexis3490 <alexispiraux72@gmail.com>
2021-01-28 12:59:06 +0100

    update manifest

--- app/src/main/java/com/estealexis/todoestealexis/task/TaskActivity.kt
+++ app/src/main/java/com/estealexis/todoestealexis/task/TaskActivity.kt
@@ -1,6 +1,8 @@
 package com.estealexis.todoestealexis.task
 
-class TaskActivity(){
+import androidx.appcompat.app.AppCompatActivity
+
+class TaskActivity: AppCompatActivity(){
     companion object {
         const val ADD_TASK_REQUEST_CODE = 666
     }
