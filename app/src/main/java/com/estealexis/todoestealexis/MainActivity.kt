b0aeaa27887eb4066516ec8864c82c3df4510785
Alexis3490 <alexispiraux72@gmail.com>
2021-01-28 13:11:31 +0100

    update task

--- app/src/main/java/com/estealexis/todoestealexis/MainActivity.kt
+++ app/src/main/java/com/estealexis/todoestealexis/MainActivity.kt
@@ -2,6 +2,9 @@ package com.estealexis.todoestealexis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.estealexis.todoestealexis.tracklist.Task
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/estealexis/todoestealexis/task/TaskActivity.kt
+++ app/src/main/java/com/estealexis/todoestealexis/task/TaskActivity.kt
@@ -1,9 +1,23 @@
 package com.estealexis.todoestealexis.task
 
+import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.estealexis.todoestealexis.R
+import com.estealexis.todoestealexis.tracklist.Task
+import java.util.*
 
 class TaskActivity: AppCompatActivity(){
     companion object {
         const val ADD_TASK_REQUEST_CODE = 666
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ass_task)
+        var valid = findViewById<Button>(R.id.button)
+        valid.setOnClickListener({
+            val newTask = Task(id = UUID.randomUUID().toString(), title = "New Task !")
+        })
     }
 }
