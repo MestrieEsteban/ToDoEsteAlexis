72f3ebd4e2b49b0d6e8b06b571ad0644e04efec0
Alexis3490 <alexispiraux72@gmail.com>
2021-02-05 10:11:04 +0100

    update

--- app/src/main/java/com/estealexis/todoestealexis/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/estealexis/todoestealexis/tasklist/TaskListFragment.kt
@@ -85,10 +85,4 @@ class TaskListFragment : Fragment(){
             if(isUpdate === null){ viewModel.addTask(task) } else { viewModel.updateTask(task) }
         }
     }
-
-    private val taskList = mutableListOf(
-        Task(id = "id_1", title = "Task 1", description = "description 1"),
-        Task(id = "id_2", title = "Task 2"),
-        Task(id = "id_3", title = "Task 3", description = "description 3")
-    )
 }
